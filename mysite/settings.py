8b396b5c4248971177422f0a457a84de625c0224
AmitModi@2021 <1986webdeveloper>
2021-03-09 12:35:41 +0530

    Update readme.md file

--- mysite/settings.py
+++ mysite/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'test',
+        'NAME': 'demo_invoice',
         'USER': 'postgres',
         'PASSWORD': 'agc123',
         'HOST': 'localhost',
