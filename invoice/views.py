b3c005a9aa3d7a2cd57e7bca2e74f03a987314db
AmitModi@2021 <1986webdeveloper>
2021-03-09 17:13:19 +0530

    template fields changes

--- invoice/views.py
+++ invoice/views.py
@@ -1,20 +1,12 @@
-from django.shortcuts import render,redirect
-from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login
+from django.shortcuts import render
 from django.contrib.auth import logout as log_out_user
-from django.http import HttpResponse,HttpResponseRedirect
-from django.contrib.auth.decorators import login_required
-from invoice.models import Invoice,InvoiceLine
+from django.http import HttpResponse, HttpResponseRedirect
+from invoice.models import Invoice, InvoiceLine
 from django.db import transaction
-from datetime import datetime
-from django.contrib.auth.hashers import make_password
 from django.views.decorators.csrf import csrf_exempt
-from django.views.generic import ListView,FormView
-from django.views.generic.edit import CreateView 
-from invoice.forms import InvoiceForm,InvoiceLineForm,InvoiceLineFormSet
-from django.forms import formset_factory
+from django.views.generic import ListView, FormView
+from invoice.forms import InvoiceForm, InvoiceLineFormSet
 from django.core.files.storage import FileSystemStorage
-from django.http import HttpResponse
 from django.template.loader import render_to_string
 from weasyprint import HTML
 import os
@@ -29,22 +21,21 @@ from django.contrib import messages
 
 
 class InvoiceListView(ListView):
-
     template_name = 'invoice/invoices.html'
     paginate_by = 10
 
     def get_queryset(self):
-        return Invoice.objects.filter(user_id = int(self.request.user.id)).order_by('-id')
+        return Invoice.objects.filter(user_id=int(self.request.user.id)).order_by('-id')
+
 
 class InvoiceCreateView(FormView):
-
     form_class = InvoiceForm
     template_name = 'invoice/invoice.html'
 
     def get_context_data(self, **kwargs):
         context = super(InvoiceCreateView, self).get_context_data(**kwargs)
         if self.request.POST:
-            context['invoice_form'] = InvoiceForm(self.request.POST,self.request.FILES)
+            context['invoice_form'] = InvoiceForm(self.request.POST, self.request.FILES)
             context['invoiceline_form'] = InvoiceLineFormSet(self.request.POST)
         else:
             context['invoice_form'] = InvoiceForm()
@@ -58,38 +49,44 @@ class InvoiceCreateView(FormView):
         invoice_line_formset = InvoiceLineFormSet(self.request.POST)
 
         if invoice_form.is_valid() and invoice_line_formset.is_valid():
-            invoice_obj = invoice_form.save(commit = False)
+            invoice_obj = invoice_form.save(commit=False)
             invoice_obj.user = self.request.user
             invoice_obj.save()
 
             for form in invoice_line_formset:
-                invoice_line_obj = form.save(commit = False)
+                invoice_line_obj = form.save(commit=False)
                 invoice_line_obj.invoice_id = invoice_obj.id
                 invoice_line_obj.save()
-            
-            make_invoice_file(self.request,invoice_obj.id)
+
+            make_invoice_file(self.request, invoice_obj.id)
             return HttpResponseRedirect('/invoices/')
+
 
 class InvoiceDetailView(View):
 
     def get(self, request, *args, **kwargs):
         invoice_id = int(self.kwargs['invoice_id'])
-        invoice_data = Invoice.objects.filter(id = int(invoice_id)).first()
-        invoice_line_data = InvoiceLine.objects.filter(invoice_id = int(invoice_id))
-        return render(request, 'invoice/invoice_detail.html', {'invoice_data': invoice_data,'invoice_line_data':invoice_line_data,'base_url':request.build_absolute_uri(invoice_data.invoice_logo.url)})
+        invoice_data = Invoice.objects.filter(id=int(invoice_id)).first()
+        invoice_line_data = InvoiceLine.objects.filter(invoice_id=int(invoice_id))
+        return render(request, 'invoice/invoice_detail.html',
+                      {'invoice_data': invoice_data, 'invoice_line_data': invoice_line_data,
+                       'base_url': request.build_absolute_uri(invoice_data.invoice_logo.url)})
+
 
 class SendMailView(View):
-    
+
     @method_decorator(csrf_exempt)
     def dispatch(self, request, *args, **kwargs):
         return super(SendMailView, self).dispatch(request, *args, **kwargs)
 
     def get(self, request, *args, **kwargs):
         invoice_id = int(self.kwargs['invoice_id'])
-        invoice_data = Invoice.objects.filter(id = invoice_id).values('invoice_num').first()
-        file_name = 'INV'+invoice_data['invoice_num']
-        mail = EmailMessage("Thank you for invoicing with us!", "A new invoice has been created on your account. You may find a PDF of your invoice attached.", settings.EMAIL_HOST_USER, [self.request.user])
-        file_path = os.path.join(settings.MEDIA_ROOT,'invoice_pdf',file_name)
+        invoice_data = Invoice.objects.filter(id=invoice_id).values('invoice_num').first()
+        file_name = 'INV' + invoice_data['invoice_num']
+        mail = EmailMessage("Thank you for invoicing with us!",
+                            "A new invoice has been created on your account. You may find a PDF of your invoice attached.",
+                            settings.EMAIL_HOST_USER, [self.request.user])
+        file_path = os.path.join(settings.MEDIA_ROOT, 'invoice_pdf', file_name)
         if os.path.exists(file_path):
             fs = FileSystemStorage(file_path)
             with fs.open(file_path) as pdf:
@@ -98,20 +95,22 @@ class SendMailView(View):
         messages.success(self.request, "Mail sent")
         return HttpResponseRedirect('/invoices/')
 
+
 class DownloadInvoiceView(View):
 
     def get(self, request, *args, **kwargs):
         invoice_id = int(self.kwargs['invoice_id'])
-        invoice_data = Invoice.objects.filter(id = invoice_id).values('invoice_num').first()
-        file_name = 'INV'+invoice_data['invoice_num']
-        file_path = os.path.join(settings.MEDIA_ROOT,'invoice_pdf',file_name)
-        
+        invoice_data = Invoice.objects.filter(id=invoice_id).values('invoice_num').first()
+        file_name = 'INV' + invoice_data['invoice_num']
+        file_path = os.path.join(settings.MEDIA_ROOT, 'invoice_pdf', file_name)
+
         if os.path.exists(file_path):
             fs = FileSystemStorage(file_path)
             with fs.open(file_path) as pdf:
                 response = HttpResponse(pdf, content_type='application/pdf')
-                response['Content-Disposition'] = 'attachment; filename= "{}"'.format(file_name) 
+                response['Content-Disposition'] = 'attachment; filename= "{}"'.format(file_name)
                 return response
+
 
 class InvoiceDeleteView(View):
 
@@ -122,10 +121,10 @@ class InvoiceDeleteView(View):
     @transaction.atomic
     def get(self, request, *args, **kwargs):
         invoice_id = int(self.kwargs['invoice_id'])
-        InvoiceLine.objects.filter(invoice_id = invoice_id).delete()
-        invoice_data = Invoice.objects.filter(id = invoice_id).first()
-        file_path = os.path.join(settings.MEDIA_ROOT,'invoice_pdf','INV'+invoice_data.invoice_num)
-        
+        InvoiceLine.objects.filter(invoice_id=invoice_id).delete()
+        invoice_data = Invoice.objects.filter(id=invoice_id).first()
+        file_path = os.path.join(settings.MEDIA_ROOT, 'invoice_pdf', 'INV' + invoice_data.invoice_num)
+
         if os.path.exists(file_path):
             os.remove(file_path)
 
@@ -133,19 +132,23 @@ class InvoiceDeleteView(View):
         messages.success(self.request, "Invoice removed sucessfully")
         return HttpResponseRedirect('/invoices/')
 
-def make_invoice_file(request,invoice_id):
-    invoice_data = Invoice.objects.filter(id = int(invoice_id)).first()
-    invoice_line_data = InvoiceLine.objects.filter(invoice_id = int(invoice_id))
-    file_path = os.path.join(settings.MEDIA_ROOT,'invoice_pdf')
-    html_string = render_to_string('invoice/download_invoice.html', {'invoice_data': invoice_data,'invoice_line_data':invoice_line_data,'base_url':request.build_absolute_uri(invoice_data.invoice_logo.url)})
+
+def make_invoice_file(request, invoice_id):
+    invoice_data = Invoice.objects.filter(id=int(invoice_id)).first()
+    invoice_line_data = InvoiceLine.objects.filter(invoice_id=int(invoice_id))
+    file_path = os.path.join(settings.MEDIA_ROOT, 'invoice_pdf')
+    html_string = render_to_string('invoice/download_invoice.html',
+                                   {'invoice_data': invoice_data, 'invoice_line_data': invoice_line_data,
+                                    'base_url': request.build_absolute_uri(invoice_data.invoice_logo.url)})
     html = HTML(string=html_string)
-    final_path = file_path +'/'+ ('INV'+invoice_data.invoice_num)
-    html.write_pdf(target= final_path)
+    final_path = file_path + '/' + ('INV' + invoice_data.invoice_num)
+    html.write_pdf(target=final_path)
     return "sucesss"
+
 
 class LoginView(View):
     def get(self, request):
-        return render(request,'invoice/login.html')
+        return render(request, 'invoice/login.html')
 
     def post(self, request):
         username = request.POST['username']
@@ -154,12 +157,13 @@ class LoginView(View):
 
         if user is not None:
             if user.is_active:
-                print(user,"user")
+                print(user, "user")
                 login(request, user)
-                return HttpResponseRedirect('/invoices') 
+                return HttpResponseRedirect('/invoices')
         else:
             messages.error(request, "Invalid username or password")
-            return render(request,'invoice/login.html')
+            return render(request, 'invoice/login.html')
+
 
 class LogoutView(View):
 
@@ -167,20 +171,21 @@ class LogoutView(View):
         log_out_user(self.request)
         return HttpResponseRedirect('/')
 
+
 class UserRegistrationView(View):
-    
+
     def get(self, request):
-        return render(request,'invoice/registration.html')
+        return render(request, 'invoice/registration.html')
 
     def post(self, request):
         username = request.POST['username']
-        password = make_password(request.POST['password'])    
+        password = make_password(request.POST['password'])
         first_name = request.POST['first_name']
 
-        if (User.objects.filter(username = username).count()) >= 1:
+        if (User.objects.filter(username=username).count()) >= 1:
             messages.error(request, "Username is already exits.")
-            return render (request,'invoice/registration.html')
+            return render(request, 'invoice/registration.html')
 
-        user = User.objects.create(username = username,password = password,first_name = first_name)
+        user = User.objects.create(username=username, password=password, first_name=first_name)
         login(request, user)
         return HttpResponseRedirect('/invoices/')
